2f897c714c5c8708228c76e7035ccc457ec2fde8
elorajn <106303933+elorajn@users.noreply.github.com>
2022-06-30 15:33:54 +0800

    Update config.js
    Signed-off-by: elorajn <106303933+elorajn@users.noreply.github.com>

--- config.js
+++ config.js
@@ -37,10 +37,10 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['27634090203','']
-global.premium = ['27634090203','918188019676']
-global.botname = 'ZIM BOT INC'
-global.ownername = 'DRIPS'
+global.owner = ['27634090203','60199915725']
+global.premium = ['27634090203','60199915725']
+global.botname = 'ELORA BOT INC'
+global.ownername = 'ELORA'
 global.group1 = 'https://chat.whatsapp.com/EFsb8RCXV4jLEFk4eAcA1A'
 global.group2 = 'https://chat.whatsapp.com/HSfcYU13g5C8GxINWwSWoA'
 global.packname = 'ZIM BOT INC'
